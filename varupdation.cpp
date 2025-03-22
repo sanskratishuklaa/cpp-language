1fce9655f8bf9e56c3353a409b0d840a9aa6d19d
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-03-22 18:27:38 +0530

    Add files via upload

---
+++ varupdation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int y=6;
+    cout<<y<<endl;
+    y=23; //Assigning new value
+    y=y+9; //y=23+9=32
+    cout<<y<<endl;
+    y*=4; //y=32*4=128
+    cout<<y<<endl;
+    y-=7; //y=128-7=121
+    cout<<y<<endl;
+}
