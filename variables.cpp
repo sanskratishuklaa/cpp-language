326d8a5d16461478264697b08d60596b589b7763
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-03-22 18:18:29 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=5; //num is a variable
+    cout<<num<<endl;
+    int z=98; //z is a variable
+    cout<<z<<endl;
+    int add = 45+6; //add is a variable
+    cout<<add<<endl;
+    float n= 45.6; //n is a variable
+    cout<<n<<endl;
+}
