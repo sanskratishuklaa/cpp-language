1dc645665457ad0954e6af76b211bd8d36c14c43
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-03-23 14:07:45 +0530

    Add files via upload

---
+++ printnum.cpp
@@ -0,0 +1,9 @@
+//write a cpp program to print the number from 1 to 10 using a for loop:
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 10;
+    for(int i=1;i<=num;i++){
+        cout<<i<<endl;
+    }
+}
