566c7c84e940768a34c2681442e33ed6b6d63717
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-03-24 21:03:19 +0530

    Add files via upload

---
+++ addnum.cpp
@@ -0,0 +1,13 @@
+// write a cpp program to addtition of two numbers:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Enter a first number: ";
+    cin >> num1;
+    cout << "Enter a second number: ";
+    cin >> num2;
+    int sum = num1 + num2;
+    cout << "Addition of entered number is: " << sum << endl;
+}
