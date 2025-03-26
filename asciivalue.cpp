6357b19e4dc9dd0c8f884cffa9c0641d59a07db6
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-03-26 15:31:51 +0530

    Add files via upload

---
+++ asciivalue.cpp
@@ -0,0 +1,12 @@
+// Take character as input and print its corresponding ASCII value:American Standard Code Information Interchange.
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+    cout << ch << endl;
+    cout << int(ch);
+    return 0;
+}
