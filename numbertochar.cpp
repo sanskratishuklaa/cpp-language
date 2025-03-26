457debca19927142e8aed1a2b8f3d283bf2e6f59
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-03-26 15:52:35 +0530

    Add files via upload

---
+++ numbertochar.cpp
@@ -0,0 +1,10 @@
+// Take an integer upto 65 as input and print its corresponding alphabet:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "Enter number: ";
+    cin >> x;
+    cout << char(x);
+}
