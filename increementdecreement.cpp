b072240f57c5a1b61ed9e15ca14015ea0166951c
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-03-27 21:02:55 +0530

    Add files via upload

---
+++ increementdecreement.cpp
@@ -0,0 +1,10 @@
+// write a cpp program that shows increement and decreement:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 7;
+    int b = 9;
+    a++, b--;
+    cout << ++a << " " << b--;
+}
