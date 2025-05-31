bad345f41aeeba3e27f1c6f84bd2cacd83269604
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-05-31 20:32:00 +0530

    Add files via upload

---
+++ variableanddatatype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 5;
+    char grade = 'A';
+    float pi = 98.65;
+    bool ispassed = true;
+    cout << age << endl;
+    cout << grade << endl;
+    cout << pi << endl;
+    cout << ispassed << endl;
+}
