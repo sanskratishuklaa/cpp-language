13b49f5e6ec873f251bd7797c332410d9bd7b3a9
Sanskrati Shukla <sanskratishukla8@gmail.com>
2025-05-31 20:36:48 +0530

    Add files via upload

---
+++ inputoutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    cout << "Enter your name: "<<endl;;
+    cin >> name;
+    cout << "Enter your age: "<<endl;;
+    cin >> age;
+    cout<<"Your name is: "<<name<<endl;
+    cout<<"Your age is: "<<age<<endl;
+
+    return 0;
+}
